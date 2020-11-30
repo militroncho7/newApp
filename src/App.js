4557097f5d1c283ced4b42c65cd7acadf890ee78
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2020-11-30 13:19:03 +0100

    chapter1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    count: 10
+  };
+
+  componentDidMount() {
+    console.log("hola mundo");
+  }
+
+  componentWillUnmount() {
+    console.log("will unmount");
+  }
+
+  handleInput = event => {
+    const value = event.target.value;
+    this.setState({count:value});
+  }
+
+  render() {
+    return(
+      <>
+        <div>El valor del contador es: {this.state.count}</div>
+        <input type="text" value={this.state.count} onChange={this.handleInput}/>
+      </>
+    )
+  }
 }
 
 export default App;
